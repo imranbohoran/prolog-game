a943e1bb037a18b53ae3abaf64fd3730802a7edf
Imran Bohoran <imran.bohoran@digital.cabinet-office.gov.uk>
2017-03-02 23:54:33 +0000

    Implementing the moving between places based on what's available.
     * And the end of a move we check if we got the treasure.
     * look, move and where_am_i is implemented

--- adventure.pl
+++ adventure.pl
@@ -3,6 +3,8 @@
 /* Dynamically changable facts */
 :-dynamic(pickedup/1).
 :-dynamic(ticket_bought/1).
+:-dynamic(current_location/1).
+:-dynamic(game_started/1).
 
 /** Facts **/
 room('Entrance hall').
@@ -62,6 +64,8 @@ ticket_bought('no').
 
 current_location('Entrance hall').
 
+game_started(false).
+
 path('Entrance hall', 'Tool shed').
 path('Tool shed', 'Entrance hall').
 path('Tool shed', 'Dark cave').
@@ -79,32 +83,43 @@ path('Puzzle room', 'Treasure room').
 
 where_am_i :-
 	current_location(Place),
-	write('You are at the '), write(Place), nl.
-
+	write('You are at the '), write(Place).
 
 look :-
 	current_location(Place),
-	write('You are at the '), write(Place), nl,
-	write('You can see : '), nl,
+	tab(2), write('You are at the '), write(Place), nl,
+	tab(2), write('You can see : '), nl,
 	notice_objects_at(Place), nl,
-	write('You can go to : '), nl,
+	tab(2), write('You can go to : '), nl,
 	where_to_go(Place).
 
 
-go(Place) :-
+move(Place) :-
 	current_location(Here),
-	path(Here, Place),
-	retract(i_am_at(Here)),
-	assert(i_am_at(Place)),
+	go(Here, Place),
+	winner.
+
+move(_) :-
+	write('You can''t move that way.').
+
+go(Here, There) :-
+	path(Here, There),
+	retract(current_location(Here)),
+	assert(current_location(There)),
 	look.
 
 go(_) :-
 	write('You can''t go that way.').
 
 
+winner :-
+	current_location('Treasure room'),
+	found_treasure.
+
+
 notice_objects_at(Place) :-
 	things_available(X, Place),
-	tab(2),
+	tab(3),
 	write('There is a '), write(X), write(' here.'), nl,
 	fail.
 notice_objects_at(_).
@@ -112,22 +127,22 @@ notice_objects_at(_).
 
 where_to_go(Place) :-
 	path(Place, X),
-	tab(2),
+	tab(3),
 	write(X),
-	nl,
-	fail.
+	nl.
+
 where_to_go(_).
 
 
 found_treasure :-
 	current_location('Treasure room'),
-	write('Congratulations, you''ve found the treasure'),
-	write('Go forth and prospoer!!!').
+	tab(2), write('Congratulations, you''ve found the treasure'),nl,
+	tab(2), write('Go forth and prospoer!!!').
 
 
 show_commands :-
 	tab(3), 
-	write('go(Place). -- Go to a place'),nl,
+	write('move(Place). -- Go to a place'),nl,
 	tab(3), 
 	write('look. -- Look around the current place'),nl,
 	tab(3),
@@ -161,6 +176,8 @@ start :- start_game.
 start_game :- 
 	intro_message,
 	nl,
+	retract(game_started(false)),
+	assert(game_started(true)),
 	write('You''re entering the Entrance hall'),
 	go('Entrance hall').
 
